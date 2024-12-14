ded8adf7ef5a11b71a5201da7f21bc25e9e247fe
Fadhly Al-farizi <74035663+fadhlyal@users.noreply.github.com>
2024-12-14 11:21:21 +0000

    test2

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import os
 
 my_variable = os.getenv("MY_VARIABLE")
+a = r'/workspaces/deploy-streamlit/lpsolve'
 
 st.write("Hello, Streamlit!")
 st.write(my_variable)
+st.write(a)
